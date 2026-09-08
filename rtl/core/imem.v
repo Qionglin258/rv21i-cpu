bfe27ee06b336611ffde8080b01421ea94f536fc
鲲鲲 <815228794@qq.com>
2026-09-08 20:03:37 +0800

    restructed

--- rtl/core/imem.v
+++ rtl/core/imem.v
@@ -5,7 +5,7 @@ module imem (
 reg [31:0] mem [255:0]; // reg for storage,256 × 32 bits
 assign rdata = mem[addr]; // read operation
 initial begin
-    $readmemh("../../../../imem.hex", mem);
+    $readmemh("imem.hex", mem);
 end
 
 endmodule
